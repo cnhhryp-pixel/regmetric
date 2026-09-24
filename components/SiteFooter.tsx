72d009f4b1bcc55c01ec24afc78f505d1bfefcb7
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-24 12:28:18 +0800

    Add shared site footer component

---
+++ components/SiteFooter.tsx
@@ -0,0 +1,8 @@
+export default function SiteFooter() {
+  return (
+    <footer className="container" style={{padding:'60px 24px'}}>
+      <p>RegMetric - Regulatory Intelligence Platform</p>
+      <p>Compliance tools, regulation database and industry guidance.</p>
+    </footer>
+  );
+}
